13415cb7b24c8349a0b973c2a79f84431c3f29f1
ricardoarodriguez <ricardoa.rodriguez@gmail.com>
2022-08-10 08:17:22 -0500

    Creación de los primeros servicios para guardar y obtener clientes

--- Project_Api/api/models.py
+++ Project_Api/api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+class Cliente(models.Model):
+    documento = models.IntegerField()
+    tipo_documento = models.CharField(max_length=20)
+    nombre = models.CharField(max_length=200, verbose_name="Nombre del Cliente")
+    genero = models.CharField(max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+class Tipo_tramite (models.Model):    
+    descripcion_tipo = models.CharField(max_length=200,null=True,blank=True)
+    nombre_tipo = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    
--- Project_Api/api/views.py
+++ Project_Api/api/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
-
+from django.views import View
+from .models import Cliente
+from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt 
+import json
 # Create your views here.
+class ClienteView(View):
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request):
+        clientes = list(Cliente.objects.values())
+        if len(clientes) > 0:
+            datos = {'message': "Success", 'clientes': clientes}
+        else:
+            datos = {'message': "Clientes no encontrados"}
+        return JsonResponse(datos)
+    
+    def post(self, request):
+        jd = json.loads(request.body)
+        print(jd)
+        Cliente.objects.create(documento=jd['documento'],tipo_documento=jd['tipo_documento'],nombre=jd['nombre'],genero=jd['genero'])
+        datos = {'message' : "Success"}
+        return JsonResponse(datos)
