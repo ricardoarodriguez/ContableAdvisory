9f70523bc22992217e630ac573dc49469f2992ab
ricardordgz <ricardoa.rodriguez@gmail.com>
2022-09-19 09:15:16 -0500

    Se agrega timedelta

--- Project_Api/Project_Api/settings.py
+++ Project_Api/Project_Api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
     # componentes para permitir conexión con frontend
     'corsheaders',
     'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
 ]
 
 MIDDLEWARE = [
