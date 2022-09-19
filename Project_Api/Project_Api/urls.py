285136743c51748fad8d652ab5d8a49497e1890b
ricardordgz <ricardoa.rodriguez@gmail.com>
2022-09-19 09:07:26 -0500

    Se agregan paths de jwt

--- Project_Api/Project_Api/urls.py
+++ Project_Api/Project_Api/urls.py
@@ -27,6 +27,10 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from api import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 router = routers.DefaultRouter()
 router.register(r'cliente', views.ClienteViewSet)
@@ -35,6 +39,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),   
 ]
 
 
